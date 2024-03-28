c355d1529015fb2e7325f322d46b9bc1d805ad6a
Vũ Mạnh Cường <160840626+Cngdz@users.noreply.github.com>
2024-03-28 09:18:32 +0700

    1

---
+++ Tuan9/Intro to Conditional Statements.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    if (n%2==1) cout<<"Weird"<<endl;
+    else if(n%2==0 && n>=6 && n<=20) cout<<"Weird"<<endl;
+    else cout<<"Not Weird"<<endl;
+}
+
